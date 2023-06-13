1fc2f157fc6907786aa402a3dfed37c50a294769
goljavi <javiergold@live.com>
2023-06-13 00:36:30 -0300

    Mas pruebas

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ with open("DESCRIPTION.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='envio_mail_action',
+    name='envio-mail-action',
     version='0.1.0',
     author='Javit0',
     author_email='javi@elgatoylacaja.com',
     description='Esto es un paquete python de prueba',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/goljavi/envio-mail-action',
+    url='https://github.com/goljavieglc/envio-mail-action',
     license='MIT',
-    packages=['envio_mail_action'],
-    install_requires=['EmailMessage', 'smtplib'],
+    packages=['envio-mail-action']
 )
